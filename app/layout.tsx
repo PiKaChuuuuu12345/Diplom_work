9f8496613390ea3e79905a8236e17715c07ce6e3
Elmyrza_Musabekov@Dev <103014439+PiKaChuuuuu12345@users.noreply.github.com>
2025-05-30 07:47:27 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,14 +26,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Подключение Font Awesome */}
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow">
-            {children}
-          </main>
+          <main className="flex-grow">{children}</main>
           <Footer />
         </div>
       </body>
