50fd49e8c4045f6d264bbe5605677dc31e6281b6
Elmyrza_Musabekov@Dev <103014439+PiKaChuuuuu12345@users.noreply.github.com>
2025-06-02 13:46:46 +0300

    Update types.ts

--- app/types.ts
+++ app/types.ts
@@ -19,3 +19,33 @@ export type Language = {
   name: string;
   flag: string;
 };
+
+declare global {
+  interface Window {
+    DG: {
+      then: (cb: () => void) => void;
+      map: (
+        id: string,
+        options: {
+          center: [number, number];
+          zoom: number;
+          fullscreenControl?: boolean;
+          zoomControl?: boolean;
+        }
+      ) => DGMap;
+      marker: (coords: [number, number]) => DGMarker;
+    };
+  }
+}
+
+interface DGMap {
+  remove: () => void;
+}
+
+interface DGMarker {
+  addTo: (map: DGMap) => DGPopup;
+}
+
+interface DGPopup {
+  bindPopup: (text: string) => void;
+}
